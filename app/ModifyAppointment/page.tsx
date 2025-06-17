5dcfc80d947482f8ede8860299eafebc066f5cf3
Alejo311548 <alejandro.vargaso@udea.edu.co>
2025-06-17 16:09:55 -0500

    commit

--- app/ModifyAppointment/page.tsx
+++ app/ModifyAppointment/page.tsx
@@ -1,6 +1,10 @@
-// app/ModifyAppointment/page.tsx
+import React, { Suspense } from "react";
 import ModifyAppointment from "@/components/organisms/ModifyAppointment";
 
 export default function Page() {
-  return <ModifyAppointment />;
+  return (
+    <Suspense fallback={<p>Cargando componente...</p>}>
+      <ModifyAppointment />
+    </Suspense>
+  );
 }
